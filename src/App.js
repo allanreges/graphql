ea0c3302d73b04f39a9f3a9958895be68c65a349
Allan Carvalho <allancarvalho@iMac-de-Suporte.local>
2020-01-22 18:00:42 -0300

    feature development

--- src/App.js
+++ src/App.js
@@ -1,72 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useQuery } from '@apollo/react-hooks';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
 const GET_POKEMON_INFO = gql`
-{
-    pokemons(first: 10) {
+  {
+    getParticipants {
       id
-      number
-      name,
-      image,
-      evolutions {
-        id,
-        number,
-        name,
-        image
+      name
+      files {
+        id
+        src
       }
     }
   }
-`
+`;
 
 function App() {
   const { data, loading, error } = useQuery(GET_POKEMON_INFO);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
+  console.log(data);
 
   return (
-  <React.Fragment>
-    <h1>Pokémons</h1>
-
-    <p>
-      <a href="https://en.wikipedia.org/wiki/List_of_Pok%C3%A9mon">
-        The Pokémon franchise
-      </a>{" "}
-      revolves around 832 fictional species of collectible monsters, each having
-      unique designs and skills. Conceived by Satoshi Tajiri in early 1989,
-      Pokémon are creatures that inhabit the fictional Pokémon World. This is
-      the list of the first 150 Pokémon as they appear in Pokémon Stadium,
-      starting with Bulbasaur in the top left corner and ending with Mewtwo in
-      the bottom right corner.
-    </p>
-    <div className="container">
-      {data &&
-        data.pokemons &&
-        data.pokemons.map((pokemon, index) => (
-          <div key={index} className="card">
-            <img src={pokemon.image} />
-            <div class="card-body">
-              <h3>{pokemon.name}</h3>
-              <p>
-                {pokemon.evolutions && pokemon.evolutions.length !== 0 && (
-                  <p>
-                    {" "}
-                    Evolutions:
-                    {pokemon.evolutions.map((e, index) => {
-                      return <p key={index}> {e.name} </p>;
-                    })}
-                  </p>
-                )}
-              </p>
-            </div>
+    <React.Fragment>
+      {data.getParticipants.map((item, index) => {
+        return (
+          <div>
+            <h1>{item.name}</h1>
+            {item.files.length > 0 && (
+              <ul
+                style={{
+                  listStyle: "none",
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  background: "#eaeaea",
+                  padding: "25px",
+                  border: "5px solid black"
+                }}
+              >
+                {item.files.map(item => {
+                  return (
+                    <li>
+                      <img
+                        style={{ maxWidth: 150, border: "5px solid #efea" }}
+                        src={item.src}
+                      ></img>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
           </div>
-        ))}
-    </div>
-  </React.Fragment>
-);
+        );
+      })}
+    </React.Fragment>
+  );
 }
 
 export default App;
